925798667c9d4d9494f55adce96d59463406e52c
thinhtruong2305 <56267717+thinhtruong2305@users.noreply.github.com>
2022-10-08 20:44:24 +0700

    Initial commit
    Trong quá trình tạo project và thêm giao diện

---
+++ Website/BookStore/BookStore.Website/Areas/Identity/Controllers/AccountController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace BookStore.Website.Areas.Identity.Controllers
+{
+    public class AccountController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
